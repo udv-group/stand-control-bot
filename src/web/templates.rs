731f42d3c2c6050654021ef84da0a59001cce5ac
goDeni <den4ikept11@gmail.com>
2024-10-08 20:42:46 +0500

    Extended hosts info

--- src/web/templates.rs
+++ src/web/templates.rs
@@ -3,7 +3,7 @@ use chrono::{DateTime, TimeDelta, Utc};
 
 use serde::{Deserialize, Serialize};
 
-use crate::db::models::{Group, GroupId, Host, HostId, LeasedHost};
+use crate::db::models::{Group, GroupId, Host, HostId, LeasedHost, User as UserDb};
 
 use super::auth::middleware::User;
 
@@ -21,14 +21,14 @@ pub struct HostsLeasePage {
     pub groups: Vec<GroupInfo>,
     pub selected_group: GroupInfo,
     pub hosts: Vec<HostInfo>,
-    pub leased: Vec<LeaseInfo>,
+    pub leased: Vec<LeasedHostInfo>,
     pub error: Option<String>,
 }
 
 #[derive(Template, Debug)]
 #[template(path = "all_hosts.html", escape = "none")]
 pub struct AllHostsPage {
-    pub hosts: Vec<LeaseInfo>,
+    pub hosts: Vec<LeasedHostInfo>,
 }
 
 #[derive(Deserialize, Debug)]
@@ -62,22 +62,62 @@ impl From<Group> for GroupInfo {
 }
 
 #[derive(Deserialize, Serialize, Debug)]
+pub struct LeaseInfo {
+    pub leased_until: DateTime<Utc>,
+    pub valid_for: String,
+    pub leased_by: String,
+}
+impl From<(UserDb, DateTime<Utc>)> for LeaseInfo {
+    fn from(value: (UserDb, DateTime<Utc>)) -> Self {
+        let (user, leased_until) = value;
+        LeaseInfo {
+            leased_by: user.login,
+            leased_until,
+            valid_for: format_duration(leased_until - Utc::now()),
+        }
+    }
+}
+
+#[derive(Deserialize, Serialize, Debug)]
 pub struct HostInfo {
     pub id: HostId,
     pub hostname: String,
     pub ip_address: String,
+    pub lease_info: Option<LeaseInfo>,
+}
+impl From<(Host, Option<UserDb>)> for HostInfo {
+    fn from(value: (Host, Option<UserDb>)) -> Self {
+        let (host, user) = value;
+
+        Self {
+            id: host.id,
+            hostname: host.hostname,
+            ip_address: host.ip_address.ip().to_string(),
+            lease_info: match (user, host.leased_until) {
+                (Some(user), Some(leased_until)) => Some((user, leased_until).into()),
+                _ => None,
+            },
+        }
+    }
 }
 impl From<Host> for HostInfo {
     fn from(value: Host) -> Self {
+        (value, None).into()
+    }
+}
+impl From<LeasedHost> for HostInfo {
+    fn from(value: LeasedHost) -> Self {
         Self {
             id: value.id,
             hostname: value.hostname,
             ip_address: value.ip_address.ip().to_string(),
+            lease_info: Some((value.user, value.leased_until).into()),
         }
     }
 }
+
 #[derive(Deserialize, Debug)]
-pub struct LeaseInfo {
+pub struct LeasedHostInfo {
     pub id: HostId,
     pub hostname: String,
     pub ip_address: String,
@@ -86,7 +126,7 @@ pub struct LeaseInfo {
     pub leased_by: String,
 }
 
-impl From<LeasedHost> for LeaseInfo {
+impl From<LeasedHost> for LeasedHostInfo {
     fn from(value: LeasedHost) -> Self {
         Self {
             id: value.id,
