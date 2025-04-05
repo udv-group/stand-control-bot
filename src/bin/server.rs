04b3a07f167330f6a2c217954a481d52798dc533
goDeni <den4ikept11@yandex.ru>
2025-04-05 14:31:49 +0500

    Message sending under traits

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,5 +1,6 @@
 use stand_control_bot::db::{run_migrations, Registry};
-use stand_control_bot::logic::notifications::BotAdapter;
+use stand_control_bot::logic::message_senders::DisabledMessageSender;
+use stand_control_bot::logic::notifications::Notifier;
 use stand_control_bot::{configuration::get_config, set_env, web::Application};
 use tracing::info;
 
@@ -7,13 +8,6 @@ use ldap3::{drive, LdapConnAsync};
 use secrecy::ExposeSecret;
 use stand_control_bot::logic::release::hosts_release_timer;
 use stand_control_bot::telemetry::init_tracing;
-
-struct EmptyBotAdapter {}
-impl BotAdapter for EmptyBotAdapter {
-    async fn send_message(&self, _user_id: i64, _msg: String) -> anyhow::Result<()> {
-        unreachable!()
-    }
-}
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
@@ -23,6 +17,7 @@ async fn main() -> Result<(), anyhow::Error> {
 
     run_migrations(&settings.database).await?;
     let registry = Registry::new(&settings.database).await?;
+    let notifier = Notifier::new(registry.clone(), DisabledMessageSender {});
 
     let (ldap_conn, ldap) =
         LdapConnAsync::with_settings(settings.ldap.clone().into(), &settings.ldap.url).await?;
@@ -54,7 +49,7 @@ async fn main() -> Result<(), anyhow::Error> {
         _ = authorized_ldap_conn_task => {
             info!("Authorized ldap connection exited")
         }
-        _ = hosts_release_timer::<EmptyBotAdapter>(registry, &None) => {
+        _ = hosts_release_timer(registry, &notifier) => {
             info!("Hosts release timer exited")
         }
     }
